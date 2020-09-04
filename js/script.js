2520b5b1036b296317c0a384c81d49b2f2c0afc4
jstloyal <jstloyalty@gmail.com>
2020-09-04 15:37:13 +0100

    Edit readme and fix lint errors

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
-let playerFactory = (name, symbol) => {
-  
-  return {name, symbol}
-}
+const playerFactory = (name, symbol) => ({ name, symbol });
 
-let board = (() => {
-  let boardArray = ['', '', '', '', '', '', '', '', ''];
+const board = (() => {
+  const boardArray = ['', '', '', '', '', '', '', '', ''];
 
-  let checkWin = (currentPlayer) => {
+  const checkWin = (currentPlayer) => {
     let ans = false;
     const WINNINGCOMBINATION = [
       [0, 1, 2],
@@ -16,81 +13,92 @@ let board = (() => {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6]
-    ]
+      [2, 4, 6],
+    ];
 
-    WINNINGCOMBINATION.forEach(arr => {
-      if(boardArray[arr[0]] == currentPlayer.symbol && boardArray[arr[1]] == currentPlayer.symbol && boardArray[arr[2]] == currentPlayer.symbol){
+    WINNINGCOMBINATION.forEach((arr) => {
+      if (
+        boardArray[arr[0]] === currentPlayer.symbol
+        && boardArray[arr[1]] === currentPlayer.symbol
+        && boardArray[arr[2]] === currentPlayer.symbol
+      ) {
         ans = true;
       }
-    })
+    });
 
     return ans;
-  }
+  };
 
-  let checkDraw = (currentPlayer) => {
-    var ans = false;
-    var count = 0;
+  const checkDraw = (currentPlayer) => {
+    let ans = false;
+    let count = 0;
 
-    boardArray.forEach(x => {
-      if(x == 'X' || x == 'O'){
-        count ++;
+    boardArray.forEach((x) => {
+      if (x === 'X' || x === 'O') {
+        count += 1;
       }
-    })
-    
-    if(!checkWin(currentPlayer) && count == 9){
+    });
+
+    if (!checkWin(currentPlayer) && count === 9) {
       ans = true;
     }
 
     return ans;
-  }
+  };
 
-  return {boardArray, checkWin, checkDraw}
-})()
+  return { boardArray, checkWin, checkDraw };
+})();
 
-let playGame = (() => {
+const playGame = () => {
   let playerOneName;
   let playerTwoName;
-  let startButton = document.getElementById('startButton');
-  let boardDiv = document.getElementById('board');
-  let resetButton = document.getElementById('resetButton');
+  const startButton = document.getElementById('startButton');
+  const boardDiv = document.getElementById('board');
+  const resetButton = document.getElementById('resetButton');
   let playerOne;
   let playerTwo;
   let currentPlayer;
 
-  function switchCurrentPlayer(boardArray){
+  function switchCurrentPlayer(boardArray) {
     let count = 0;
 
-    for(let i = 0; i < boardArray.length; i++){
-      if(boardArray[i] === 'X' || boardArray[i] === 'O'){
-        count++;
+    for (let i = 0; i < boardArray.length; i += 1) {
+      if (boardArray[i] === 'X' || boardArray[i] === 'O') {
+        count += 1;
       }
     }
 
-    count % 2 === 0 ? currentPlayer = playerOne : currentPlayer = playerTwo;
+    if (count % 2 === 0) {
+      currentPlayer = playerOne;
+    } else {
+      currentPlayer = playerTwo;
+    }
   }
 
-  function displayPlayerTurn(currentPlayer){
-    document.getElementById('playerTurn').innerHTML = `${currentPlayer.name}, it is your turn.`
+  function displayPlayerTurn(currentPlayer) {
+    document.getElementById(
+      'playerTurn',
+    ).innerHTML = `${currentPlayer.name}, it is your turn.`;
   }
 
   boardDiv.addEventListener('click', (e) => {
-    if(e.target.innerHTML == ""){
-      var index = e.target.getAttribute('data-index');
+    if (e.target.innerHTML === '') {
+      const index = e.target.getAttribute('data-index');
       e.target.innerHTML = currentPlayer.symbol;
-      board.boardArray[index] = currentPlayer.symbol
+      board.boardArray[index] = currentPlayer.symbol;
 
-      if(board.checkWin(currentPlayer)){
-        document.getElementById('playerTurn').innerHTML = `${currentPlayer.name}, has won the game..`
-      } else if(board.checkDraw(currentPlayer)){
-        document.getElementById('playerTurn').innerHTML = 'The game is a tie.'
-      }else {
-        switchCurrentPlayer(board.boardArray)
-        displayPlayerTurn(currentPlayer)
+      if (board.checkWin(currentPlayer)) {
+        document.getElementById(
+          'playerTurn',
+        ).innerHTML = `${currentPlayer.name}, has won the game..`;
+      } else if (board.checkDraw(currentPlayer)) {
+        document.getElementById('playerTurn').innerHTML = 'The game is a tie.';
+      } else {
+        switchCurrentPlayer(board.boardArray);
+        displayPlayerTurn(currentPlayer);
       }
-
     }
-  })
+  });
 
   startButton.addEventListener('click', () => {
     playerOneName = document.getElementById('player1').value;
@@ -99,25 +107,29 @@ let playGame = (() => {
     document.getElementById('inputsContainer').style.display = 'none';
     document.getElementById('gameDetails').style.display = 'block';
 
-    document.getElementById('player1').value = "";
-    document.getElementById('player2').value = "";
+    document.getElementById('player1').value = '';
+    document.getElementById('player2').value = '';
 
     playerOne = playerFactory(playerOneName, 'X');
     playerTwo = playerFactory(playerTwoName, 'O');
 
-    
-    document.getElementById('playerOneName').innerHTML = `${playerOne.name}(${playerOne.symbol})`
-    document.getElementById('playerTwoName').innerHTML = `${playerTwo.name}(${playerTwo.symbol})`
+    document.getElementById(
+      'playerOneName',
+    ).innerHTML = `${playerOne.name}(${playerOne.symbol})`;
+    document.getElementById(
+      'playerTwoName',
+    ).innerHTML = `${playerTwo.name}(${playerTwo.symbol})`;
 
     currentPlayer = playerOne;
 
-    document.getElementById('playerTurn').innerHTML = `${currentPlayer.name}, it is you turn.`
-  })
+    document.getElementById(
+      'playerTurn',
+    ).innerHTML = `${currentPlayer.name}, it is you turn.`;
+  });
 
   resetButton.addEventListener('click', () => {
     document.location.reload();
-  })
+  });
+};
 
-})()
-
-playGame
+playGame();
