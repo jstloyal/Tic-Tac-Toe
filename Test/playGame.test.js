4102fb23a736b8c9a73328eaa5df4cdbee003def
jstloyal <jstloyalty@gmail.com>
2020-09-29 21:32:52 +0100

    Update test cases

---
+++ Test/playGame.test.js
@@ -0,0 +1,1 @@
+import playGame from '../js/playGame';
--- Test/playerFactory.test.js
+++ Test/playerFactory.test.js
@@ -3,3 +3,7 @@ import playerFactory from '../js/playerFactory'
 test("It returns an object containing player name and symbol", () => {
   expect(playerFactory('Sam', 'X')).toEqual({name: 'Sam', symbol: 'X'})
 })
+
+test("It returns an object containing player name and symbol", () => {
+  expect(playerFactory('Ola', 'O')).toEqual({name: 'Ola', symbol: 'O'})
+})
