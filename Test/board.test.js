aade7bc139671665fe745e83ca9b3c1e9698a77c
ebeagusamuel <samuelebeagu@gmail.com>
2020-09-29 16:19:10 +0100

    test checkdraw method

--- Test/board.test.js
+++ Test/board.test.js
@@ -1,6 +1,6 @@
 import board from "../js/board";
 
-const { boardArray, updateBoardArray, checkWin } = board;
+const { boardArray, updateBoardArray, checkWin, checkDraw } = board;
 
 test("It return an array of length 9, with empty string", () => {
   expect(boardArray).toEqual(["", "", "", "", "", "", "", "", ""]);
@@ -13,16 +13,38 @@ test("It updates the board array", () => {
 });
 
 describe("It checks for winning combination in the board array", () => {
-  test('it return false when there is no winning combination in the board array', () => {
-    let currentPlayer =  { name: "Sam", symbol: "X" };
-    expect(checkWin(currentPlayer)).toBeFalsy()
-  })
+  test("it return false when there is no winning combination in the board array", () => {
+    let currentPlayer = { name: "Sam", symbol: "X" };
+    expect(checkWin(currentPlayer)).toBeFalsy();
+  });
 
-  test('it return true when there is winning combination in the board array', () => {
-    let currentPlayer =  { name: "Sam", symbol: "X" };
-    for(let i = 0; i < 3; i += 1){
-      updateBoardArray(currentPlayer, i)
+  test("it return true when there is winning combination in the board array", () => {
+    let currentPlayer = { name: "Sam", symbol: "X" };
+    for (let i = 0; i < 3; i += 1) {
+      updateBoardArray(currentPlayer, i);
     }
-    expect(checkWin(currentPlayer)).toBeTruthy()
-  })
-})
+    expect(checkWin(currentPlayer)).toBeTruthy();
+  });
+});
+
+describe("It checks if the game is a draw", () => {
+  test("It return false if the game is not a draw", () => {
+    let currentPlayer = { name: "Sam", symbol: "X" };
+    expect(checkDraw(currentPlayer)).toBeFalsy();
+  });
+
+  test("It return true when the game is a draw", () => {
+    let player1 = { name: "Sam", symbol: "X" };
+    let player2 = { name: "Tom", symbol: "O" };
+    [0, 2, 3, 7, 8]
+      .forEach((x) => {
+        updateBoardArray(player1, x);
+      });
+
+    [1, 4, 5, 6].forEach((x) => {
+      updateBoardArray(player2, x);
+    });
+
+    expect(checkDraw(player2)).toBeTruthy()
+  });
+});
