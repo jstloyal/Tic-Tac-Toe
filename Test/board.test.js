f0fd15c918720870f559728ee67324db0e070407
jstloyal <jstloyalty@gmail.com>
2020-09-30 03:09:26 +0100

    Fix lint errors

--- Test/board.test.js
+++ Test/board.test.js
@@ -1,25 +1,27 @@
-import board from "../js/board";
+import board from '../js/board';
 
-const { boardArray, updateBoardArray, checkWin, checkDraw } = board;
+const {
+  boardArray, updateBoardArray, checkWin, checkDraw,
+} = board;
 
-test("It return an array of length 9, with empty string", () => {
-  expect(boardArray).toEqual(["", "", "", "", "", "", "", "", ""]);
+test('It return an array of length 9, with empty string', () => {
+  expect(boardArray).toEqual(['', '', '', '', '', '', '', '', '']);
 });
 
-test("It updates the board array", () => {
-  const currentPlayer = { name: "Sam", symbol: "X" };
+test('It updates the board array', () => {
+  const currentPlayer = { name: 'Sam', symbol: 'X' };
   updateBoardArray(currentPlayer, 0);
-  expect(boardArray).toEqual(["X", "", "", "", "", "", "", "", ""]);
+  expect(boardArray).toEqual(['X', '', '', '', '', '', '', '', '']);
 });
 
-describe("It checks for winning combination in the board array", () => {
-  test("it return false when there is no winning combination in the board array", () => {
-    const currentPlayer = { name: "Sam", symbol: "X" };
+describe('It checks for winning combination in the board array', () => {
+  test('it return false when there is no winning combination in the board array', () => {
+    const currentPlayer = { name: 'Sam', symbol: 'X' };
     expect(checkWin(currentPlayer)).toBeFalsy();
   });
 
-  test("it return true when there is winning combination in the board array", () => {
-    const currentPlayer = { name: "Sam", symbol: "X" };
+  test('it return true when there is winning combination in the board array', () => {
+    const currentPlayer = { name: 'Sam', symbol: 'X' };
     for (let i = 0; i < 3; i += 1) {
       updateBoardArray(currentPlayer, i);
     }
@@ -27,15 +29,15 @@ describe("It checks for winning combination in the board array", () => {
   });
 });
 
-describe("It checks if the game is a draw", () => {
-  test("It return false if the game is not a draw", () => {
-    const currentPlayer = { name: "Sam", symbol: "X" };
+describe('It checks if the game is a draw', () => {
+  test('It return false if the game is not a draw', () => {
+    const currentPlayer = { name: 'Sam', symbol: 'X' };
     expect(checkDraw(currentPlayer)).toBeFalsy();
   });
 
-  test("It return true when the game is a draw", () => {
-    const player1 = { name: "Sam", symbol: "X" };
-    const player2 = { name: "Tom", symbol: "O" };
+  test('It return true when the game is a draw', () => {
+    const player1 = { name: 'Sam', symbol: 'X' };
+    const player2 = { name: 'Tom', symbol: 'O' };
     [0, 2, 3, 7, 8].forEach((x) => {
       updateBoardArray(player1, x);
     });
--- js/playGame.js
+++ js/playGame.js
@@ -1,12 +1,12 @@
-import board from "./board";
-import playerFactory from "./playerFactory";
+import board from './board';
+import playerFactory from './playerFactory';
 
 const playGame = () => {
   let playerOneName;
   let playerTwoName;
-  const startButton = document.getElementById("startButton");
-  const boardDiv = document.getElementById("board");
-  const resetButton = document.getElementById("resetButton");
+  const startButton = document.getElementById('startButton');
+  const boardDiv = document.getElementById('board');
+  const resetButton = document.getElementById('resetButton');
   let playerOne;
   let playerTwo;
   let currentPlayer;
@@ -15,7 +15,7 @@ const playGame = () => {
     let count = 0;
 
     for (let i = 0; i < boardArray.length; i += 1) {
-      if (boardArray[i] === "X" || boardArray[i] === "O") {
+      if (boardArray[i] === 'X' || boardArray[i] === 'O') {
         count += 1;
       }
     }
@@ -29,25 +29,25 @@ const playGame = () => {
 
   function displayPlayerTurn(currentPlayer) {
     document.getElementById(
-      "playerTurn"
+      'playerTurn',
     ).innerHTML = `${currentPlayer.name}, it is your turn.`;
   }
 
   const play = (event) => {
-    const index = event.target.getAttribute("data-index");
+    const index = event.target.getAttribute('data-index');
 
-    if (board.boardArray[index] === "") {
+    if (board.boardArray[index] === '') {
       board.updateBoardArray(currentPlayer, index);
       board.render();
 
       if (board.checkWin(currentPlayer)) {
         document.getElementById(
-          "playerTurn"
+          'playerTurn',
         ).innerHTML = `${currentPlayer.name}, has won the game..`;
-        boardDiv.removeEventListener("click", play);
+        boardDiv.removeEventListener('click', play);
       } else if (board.checkDraw(currentPlayer)) {
-        document.getElementById("playerTurn").innerHTML = "The game is a tie.";
-        boardDiv.removeEventListener("click", play);
+        document.getElementById('playerTurn').innerHTML = 'The game is a tie.';
+        boardDiv.removeEventListener('click', play);
       } else {
         switchCurrentPlayer(board.boardArray);
         displayPlayerTurn(currentPlayer);
@@ -55,36 +55,36 @@ const playGame = () => {
     }
   };
 
-  boardDiv.addEventListener("click", play);
+  boardDiv.addEventListener('click', play);
 
-  startButton.addEventListener("click", () => {
-    playerOneName = document.getElementById("player1").value;
-    playerTwoName = document.getElementById("player2").value;
+  startButton.addEventListener('click', () => {
+    playerOneName = document.getElementById('player1').value;
+    playerTwoName = document.getElementById('player2').value;
 
-    document.getElementById("inputsContainer").style.display = "none";
-    document.getElementById("gameDetails").style.display = "block";
+    document.getElementById('inputsContainer').style.display = 'none';
+    document.getElementById('gameDetails').style.display = 'block';
 
-    document.getElementById("player1").value = "";
-    document.getElementById("player2").value = "";
+    document.getElementById('player1').value = '';
+    document.getElementById('player2').value = '';
 
-    playerOne = playerFactory(playerOneName, "X");
-    playerTwo = playerFactory(playerTwoName, "O");
+    playerOne = playerFactory(playerOneName, 'X');
+    playerTwo = playerFactory(playerTwoName, 'O');
 
     document.getElementById(
-      "playerOneName"
+      'playerOneName',
     ).innerHTML = `${playerOne.name}(${playerOne.symbol})`;
     document.getElementById(
-      "playerTwoName"
+      'playerTwoName',
     ).innerHTML = `${playerTwo.name}(${playerTwo.symbol})`;
 
     currentPlayer = playerOne;
 
     document.getElementById(
-      "playerTurn"
+      'playerTurn',
     ).innerHTML = `${currentPlayer.name}, it is you turn.`;
   });
 
-  resetButton.addEventListener("click", () => {
+  resetButton.addEventListener('click', () => {
     document.location.reload();
   });
 };
